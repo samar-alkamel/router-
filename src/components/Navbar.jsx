dde45796b7fd4187a955bc8e2d0245c56ce12fc7
samar <Sa1617778sa@gmail.com>
2021-02-04 17:06:00 +0200

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar(props) {
+    return (
+        <div className ='navbar'>
+          Home
+           < div className ='navbar-left' >
+               Admin Dashoard 
+               <button>login</button>
+               </ div>
+        </div>
+    );
+}
+
+export default Navbar;
